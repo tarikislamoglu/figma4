2019f863403be683c9a0a9dd1f3ad7e5794e5afd
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-05-08 13:02:36 +0300

    finalll

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 const App = () => {
   return (
     <div className=" relative h-screen w-full flex justify-center items-center bg-[#02004FED]">
-      <div className="w-1/3 h-screen bg-[#363d7b42] absolute top-0 right-0"></div>
+      <img
+        src="/Polygon 2.png"
+        className="absolute top-10 right-0 pb-12 w-1/3 h-screen "
+      />
+
       <div className="flex flex-col w-1/3 space-y-5 pl-5">
         <h1 className="w-1/2 text-[50px] font-bold text-[#FFFFFF80] h-[85px] ">
           LOGIN
@@ -20,7 +24,11 @@ const App = () => {
           Login
         </button>
       </div>
-      <div className="w-1/3 h-screen bg-[#363d7b42] absolute top-0 left-0"></div>
+
+      <img
+        src="/Polygon 1.svg"
+        className="absolute top-10 left-0 pb-12 w-1/3 h-screen "
+      />
     </div>
   );
 };
